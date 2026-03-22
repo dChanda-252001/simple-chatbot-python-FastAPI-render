d9d589936cc78a7d486f57fcffd63a1f0a6359a1
Durbar Chanda <anuragchanda251001@gmail.com>
2026-03-23 03:19:42 +0530

    modified:   frontend/script.js

--- frontend/script.js
+++ frontend/script.js
@@ -19,7 +19,7 @@ async function sendMessage() {
     input.value = "";
 
     // API Call
-    let response = await fetch("http://127.0.0.1:8000/chat", {
+    let response = await fetch("https://simple-chatbot-python-fastapi-render.onrender.com/chat", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
